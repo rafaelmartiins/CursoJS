3b9bb1cd93e693a7437ac435c2971f1dc92fa3f0
Rafael Martins <martns.rf@gmail.com>
2023-06-23 19:43:49 -0300

    arrays

---
+++ JS Basico/aula6/js/number.js
@@ -0,0 +1,24 @@
+// let num1 = 12.845416;
+// let num2 = 2.5;
+
+// console.log(num1.toString() + num2); // transformar numero em uma string, mas ele ainda continuar tipo number
+// console.log(num1.toString(2));  //binário
+// console.log(num1.toFixed(2)); // Redonda o numero grande 12.845416 para 12.85
+// console.log(Number.isInteger(num1)); // retorna se o numero é inteiro.   
+
+// ####################################
+
+// OBJETO MATH
+
+//
+
+let num1 = 9;
+console.log(num1 ** 0.5);
+
+
+// let num2 = Math.floor(num1); redonda para baixo
+// let num2 = Math.ceil(num1); Redonda para cima
+// let num2 = Math.round(num1);  Redonda para o mais proximo
+// console.log(Math.max(1,2,3,4,5,-10,-5,1500,200));
+// console.log(Math.min(1,2,3,4,5,-10,-5,1500,200));
+// const aleat = Math.random() * (10 - 5) + 5;
---
+++ JS Basico/aula7/arrays.js
@@ -0,0 +1,17 @@
+let alunos = ['Rafael', 'Nicolly', 'Luk'];
+alunos.push('Trunks');
+alunos.push('Goten');
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+
+// alunos.push('Jordan'); // adiciona no fim do array
+// alunos.unshift('James');  // adiciona no começo do array
+// alunos.shift(); // remove no começo
+// alunos.pop(); // remove no fim
+
+// delete alunos[1]; // deleta um indice especifico
+
+// console.log(alunos.slice(1, 4)); // selecionar um indice ao outro espefico
