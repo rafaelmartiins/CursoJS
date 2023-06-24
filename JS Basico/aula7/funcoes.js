1dc22315968dba41cb65c77787574270bb652b06
Rafael Martins <martns.rf@gmail.com>
2023-06-24 11:32:57 -0300

    funcoes

---
+++ JS Basico/aula7/funcoes.js
@@ -0,0 +1,38 @@
+// função moderna - Arrow Function
+
+
+
+const raiz1 = n => n ** 0.5; // quando tem apenas 1 parametro
+console.log(raiz1(9));
+
+
+const raiz = (x, y) => {
+    return x + y;
+};
+
+console.log(raiz(10, 5));
+
+
+
+
+
+
+/*const raiz = function(n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+*/
+
+
+/*
+function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma(4, 6);
+console.log(resultado);
+*/
